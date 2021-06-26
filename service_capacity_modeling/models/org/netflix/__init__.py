0c2a137fda586ccd20d7c0e3780d325b2b4bc569
rshringi <rshringi@netflix.com>
2021-06-25 17:06:37 -0700

    Initial commit for TimeSeries

--- service_capacity_modeling/models/org/netflix/__init__.py
+++ service_capacity_modeling/models/org/netflix/__init__.py
@@ -1,6 +1,7 @@
 from .cassandra import nflx_cassandra_capacity_model
 from .evcache import nflx_evcache_capacity_model
 from .key_value import nflx_key_value_capacity_model
+from .time_series import nflx_time_series_capacity_model
 from .rds import nflx_rds_capacity_model
 from .stateless_java import nflx_java_app_capacity_model
 from .zookeeper import nflx_zookeeper_capacity_model
@@ -11,6 +12,7 @@ def models():
         "org.netflix.cassandra": nflx_cassandra_capacity_model,
         "org.netflix.stateless-java": nflx_java_app_capacity_model,
         "org.netflix.key-value": nflx_key_value_capacity_model,
+        "org.netflix.timeseries": nflx_time_series_capacity_model,
         "org.netflix.zookeeper": nflx_zookeeper_capacity_model,
         "org.netflix.evcache": nflx_evcache_capacity_model,
         "org.netflix.rds": nflx_rds_capacity_model
