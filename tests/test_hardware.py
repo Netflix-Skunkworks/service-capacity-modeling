ea99efe23a13c853767faa6588d26b6a5e9573ea
Jason Koch <jkoch@netflix.com>
2025-01-31 11:04:37 -0800

    Improve pricing merge readability, add tests, skip test sensitive to sort

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -15,3 +15,12 @@ def test_loaded_from_ec2_and_overrides():
     assert ec2.annual_cost > 0
 
     assert shapes.region("us-east-1").zones_in_region == 3
+
+
+def test_overrides_correct_order():
+    m6idxl = shapes.region("us-east-1").instances["m6id.xlarge"]
+    m6id4xl = shapes.region("us-east-1").instances["m6id.4xlarge"]
+
+    # these are in the overrides file
+    assert m6idxl.annual_cost == 781.66
+    assert m6id4xl.annual_cost == 3126.64
