b544f7c2ab84f102073061153e425151ca7476e0
Jason Koch <jkoch@netflix.com>
2025-01-30 18:31:26 -0800

    Add tool to fetch latest EC2 pricing from AWS API
    Introduces a tool to automatically fetch EC2 pricing and inject into the
    hardware pricing information for AWS. Also, update the hardware profile
    loading to allow multiple files, read by lexicographical sort.

--- service_capacity_modeling/hardware/profiles/__init__.py
+++ service_capacity_modeling/hardware/profiles/__init__.py
@@ -6,11 +6,23 @@ except ImportError:
 
     import_module = __import__  # type: ignore[assignment]
 
+from typing import Dict, List, Tuple
 from service_capacity_modeling.hardware import load_hardware_from_disk
 from pathlib import Path
 
 current_module = import_module(__name__)
 common_profiles = {}
+
+
+def group_profile_paths(shapes_path: Path) -> Dict[str, List[Path]]:
+    groups = {}
+    for f in sorted(shapes_path.glob("**/*.json")):
+        prefix = f.stem.split("_")[0]
+        if prefix not in groups:
+            groups[prefix] = []
+        groups[prefix].append(f)
+    return groups
+
 
 with resources.path(  # pylint: disable=deprecated-method
     current_module, "profiles.txt"
@@ -20,7 +32,8 @@ with resources.path(  # pylint: disable=deprecated-method
         shape = fd.stem
 
         print(f"Loading shape={shape} from {Path(shapes, shape)}")
-        for pricing in Path(shapes.parent, "pricing", shape).glob("**/*.json"):
-            print(f"Loading {pricing}")
-            ghw = load_hardware_from_disk(price_path=pricing, shape_path=fd)
-            common_profiles[f"{shape}-{pricing.stem}"] = ghw
+        groups = group_profile_paths(Path(shapes.parent, "pricing", shape))
+        for pricing_name, pricing_paths in groups.items():
+            print(f"Loading {pricing_name} -> {pricing_paths}")
+            ghw = load_hardware_from_disk(price_paths=pricing_paths, shape_path=fd)
+            common_profiles[f"{shape}-{pricing_name}"] = ghw
--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -6,3 +6,12 @@ def test_blob():
     assert s3.annual_cost_gib(1) > 0
     assert s3.annual_cost_per_write_io > 0
     assert s3.annual_cost_per_read_io > 0
+
+
+def test_loaded_from_ec2_and_overrides():
+    ec2 = shapes.region("us-east-1").instances["m5.large"]
+    assert ec2.annual_cost > 0
+    ec2 = shapes.region("us-east-1").instances["db.r5.large"]
+    assert ec2.annual_cost > 0
+
+    assert shapes.region("us-east-1").zones_in_region == 3
