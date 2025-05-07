6af0b69ef1fb7f6818d3be4a4375d375e2d72ea0
Jason Koch <jkoch@netflix.com>
2025-05-06 17:46:43 -0700

    realign ipc numbers for icelake and sapphire rapids

--- service_capacity_modeling/tools/instance_families.py
+++ service_capacity_modeling/tools/instance_families.py
@@ -6,8 +6,8 @@
 
 HSW_IPC = 0.85
 SKX_IPC = 1.0
-ICX_IPC = SKX_IPC * 1.18 # harshad says 1.15, chatgpt says 1.18
-SPR_IPC = ICX_IPC * 1.19 # harshad says 1.12, chatgpt says 1.19
+ICX_IPC = SKX_IPC * 1.15 # harshad says 1.15, chatgpt says 1.18
+SPR_IPC = ICX_IPC * 1.12 # harshad says 1.12, chatgpt says 1.19
 ROME_IPC = 1.03
 MILAN_IPC = SKX_IPC * 1.15
 GENOA_IPC = MILAN_IPC * 1.13
